393a20a9ae7875e6f25bdcd8457cf75ada3946b2
ortish0-debug <ortish0@gmail.com>
2026-05-27 03:14:55 +0300

    yt

--- server.ts
+++ server.ts
@@ -99,9 +99,14 @@ async function startServer() {
 
   // CORS Middleware to allow requests from axiom-consult.ru and other environments
   app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    const origin = req.headers.origin;
+    if (origin) {
+      res.setHeader("Access-Control-Allow-Origin", origin);
+    } else {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+    }
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,Authorization");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     
     if (req.method === "OPTIONS") {
